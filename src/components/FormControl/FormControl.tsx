8baddd5efb744fb34053d06c3be22c58145744f1
Rodolfo Rodríguez <rdf.rdz.11@gmail.com>
2023-03-06 21:06:48 -0600

    FormControl component

---
+++ src/components/FormControl/FormControl.tsx
@@ -0,0 +1,32 @@
+import { FormHelperText, Grid, GridProps } from "@mui/material"
+import { useStyles } from "./FormControl.styles"
+
+export type FormControlProps = Omit<GridProps, 'onChange'> & {
+    /**
+     * Error message to display
+     */
+    error?: string
+    /**
+     * If the input was 'touched' (focused, then blurred)
+     */
+    touched?: boolean
+}
+
+export function FormControl({ children, error, touched, className, ...rest }: FormControlProps) {
+    const { classes, cx } = useStyles()
+
+    return (
+        <Grid
+            id="form-control"
+            container
+            className={cx(classes.formControl, className)}
+            {...rest}
+
+        >
+            {children}
+            {Boolean(error) && touched && (
+                <FormHelperText> {error} </FormHelperText>
+            )}
+        </Grid>
+    )
+}
