7a59fd4eb26f7ac341dd358ef802516987ccf211
Rodolfo Rodríguez <rdf.rdz.11@gmail.com>
2023-03-13 19:48:21 -0600

    inputTypes

--- src/helpers/inputTypes.ts
+++ src/helpers/inputTypes.ts
@@ -4,12 +4,16 @@ import {
     TextInput,
     SelectInput,
     EmailInput,
-    CheckInput
+    CheckInput,
+    DateInput
 } from "src/components/Inputs"
+import { RadioInput } from "src/components/Inputs/Radio";
 
 export const inputTypes: { [key in InputTypes]: ElementType } = Object.freeze({
     text: TextInput,
     select: SelectInput,
     email: EmailInput,
-    check: CheckInput
+    check: CheckInput,
+    date: DateInput,
+    radio: RadioInput
 })
